fixing erros and f calculation

src/models/carnahan_starling.py
@@ -5,13 +5,13 @@ class CarnahanStarling():
     def __init__(self, diameter: Union[float, np.ndarray]):
         self.__diameter = diameter
         self.__phi = np.array([
-            [7.5365557, -359.44, 1550.9, -1911.28, 9236.9, 10],
-            [-37.60463, 1825.6, -5070.1, 9.063632, 21390.175, -129430, 10],
-            [71.745953, -3168, 6534.6, -17.94482, -51320.7, 357230, 0.57],
-            [-46.83552, 1884.2, -3288.7, 11.34027, 37064.54, -315530, -6.7],
-            [-2.467982, -0.82376, -2.7171, 20.52142, 1103.742, 1390.2, -8],
-            [-0.50272, -3.1935, 2.0883, -56.6377, -3264.61, 4518.2, 0],
-            [8.0956883, 3.7090, 0, 40.53683, 2556.181, 4241.6, 0]
+            [7.5365557, -359.44, 1550.9, -1.19932, -1911.28, 9236.9],
+            [-37.60463, 1825.6, -5070.1, 9.063632, 21390.175, -129430],
+            [71.745953, -3168, 6534.6, -17.94482, -51320.7, 357230],
+            [-46.83552, 1884.2, -3288.7, 11.34027, 37064.54, -315530],
+            [-2.467982, -0.82376, -2.7171, 20.52142, 1103.742, 1390.2],
+            [-0.50272, -3.1935, 2.0883, -56.6377, -3264.61, -4518.2],
+            [8.0956883, 3.7090, 0, 40.53683, 2556.181, 4241.6]
         ], dtype=float)
 
     @property
@@ -47,10 +47,25 @@ class CarnahanStarling():
         alpha = C*((1/(lambda_a-3))-(1/(lambda_r-3)))
         return alpha
 
-    def f_i(self, alpha: Union[float, np.ndarray], i: int) -> Union[float, np.ndarray]:
-        phi_i = self.phi[i, :]
-        f_i = (
-            (phi_i[0] + phi_i[1]*(alpha**1) + phi_i[2]*(alpha**2) + phi_i[3]*(alpha**3))
-            /(1 + phi_i[4]*alpha + phi_i[5]*(alpha**2) + phi_i[6]*(alpha**3))
-        )
-        return f_i
+    def __alpha_powers(self, alpha: Union[float, np.ndarray]) -> np.ndarray:
+        # returns an np.ndarray with shape (4, length_alpha)
+        if not isinstance(alpha, np.ndarray):
+            alpha = np.array([alpha])
+        
+        powers = np.arange(4)
+        alpha_col = alpha[:, np.newaxis]
+        result = alpha_col**powers
+        return result.T
+    
+    def f(self, alpha: Union[float, np.ndarray]) -> np.ndarray:
+
+        alpha_powers = self.__alpha_powers(alpha)
+
+        # (6 x 4) x (4, length_alpha) = (6 x length_alpha)
+        numerator = np.matmul(self.phi[:4, :].T*alpha_powers)
+        # (6 x 3) x (3, length_alpha) = (6 x length_alpha)
+        demonimator = 1 + np.matmul(self.phi[4:, :].T*alpha_powers[1:, :])
+
+        # final result, shape: (6 x length_alpha)
+        f = (numerator/demonimator)
+        return f