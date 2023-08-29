e49ddf12cb98162d0bc10daebd6001983526720b
rodolfoamancio <rodolfojamancio@gmail.com>
2023-08-29 20:27:36 -0300

    constants update

---
+++ saftvrmie/constants/__init__.py
@@ -0,0 +1,6 @@
+from .constants import (
+    BOLTZMANN,
+    AVOGADRO,
+    ANGSTRON,
+    KILOGRAM
+)
--- saftvrmie/constants/constants.py
+++ saftvrmie/constants/constants.py
@@ -1,4 +1,8 @@
 
+# physical constants
 BOLTZMANN = 1.3806452E-23
 AVOGADRO = 6.023E23
+
+# conversion factors
 ANGSTRON = 1E-10
+KILOGRAM = 1E3
