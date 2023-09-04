08e13aaf44f9cc5a61a70b8f98a036a39b011146
rodolfoamancio <rodolfojamancio@gmail.com>
2023-09-04 19:33:26 -0300

    reader documentation

--- saftvrmie/files/reader.py
+++ saftvrmie/files/reader.py
@@ -4,48 +4,87 @@ import yaml
 from typing import Union
 
 class Reader():
+    """
+    Class for YAML reader
+    """
     @property
     def yaml_path(self) -> str:
         return self.__yaml_path
     
     @property
     def segment_diameter(self) -> float:
+        """
+        Get the segment diameter in A
+        """
         return self.__segment_diameter
     
     @property
     def potential_depth(self) -> float:
+        """
+        Get the potential depth in K
+        """
         return self.__potential_depth
     
     @property
     def repulsive_exponent(self) -> float:
+        """
+        Get the repulsive exponent
+        """
         return self.__repulsive_exponent
     
     @property
     def attractive_exponent(self) -> float:
+        """
+        Get the attractive exponent
+        """
         return self.__attractive_exponent
     
     @property
     def ms(self) -> float:
+        """
+        Get the number of segments per molecule
+        """
         return self.__ms
     
     @property
     def molar_mass(self) -> float:
+        """
+        Get the molar mass in g/mol
+        """
         return self.__molar_mass
     
     @property
     def temperature(self) -> Union[float, np.ndarray]:
+        """
+        Get the temperature in K, either a single value or an array
+        """
         return self.__temperature
     
     @property
     def density(self) -> Union[float, np.ndarray]:
+        """
+        Get the density in kg/m³, either a single value or an array
+        """
         return self.__density
     
     @property
     def output_filename(self) -> str:
+        """
+        Get the output filename
+        """
         return self.__output_filename
 
     @staticmethod
     def read(yaml_path: str) -> 'Reader': 
+        """
+        Method for reading a yaml input file
+
+        Parameters:
+        - yaml_path: str - The path to the input file
+
+        Returns:
+        - reader: Reader - a reader object with the relevant data
+        """
         reader = Reader()
         reader.__yaml_path = yaml_path
         with open(yaml_path, "r") as file:
