7b2198cfdf7a805f850aa3546f2c94e5ad63de41
rodolfoamancio <rodolfojamancio@gmail.com>
2023-09-04 19:33:11 -0300

    added constants units

--- saftvrmie/constants/constants.py
+++ saftvrmie/constants/constants.py
@@ -1,8 +1,8 @@
 
 # physical constants
-BOLTZMANN = 1.3806452E-23
-AVOGADRO = 6.023E23
+BOLTZMANN = 1.3806452E-23 # J/K
+AVOGADRO = 6.023E23 # -
 
 # conversion factors
-ANGSTRON = 1E-10
-KILOGRAM = 1E3
+ANGSTRON = 1E-10 # m
+KILOGRAM = 1E3 # g
